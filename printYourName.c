37beed0477d0fe323c3150acd56630433e744dd7
fatihtaskinnn <119786773+fatihtaskinnn@users.noreply.github.com>
2022-12-13 10:07:40 +0300

    Fatih Taşkın

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,32 @@
 // your name surname and age from user
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	char name[10],surname[20];
+	int c;
+	
+	printf("please enter your name.");
+	scanf("%s",&name);
+	
+	printf("please enter your surname.");
+	scanf("%s",&surname);
+	
+	printf("please enter your age");
+	scanf("%d",&c);
+	
+	printf("%s %s %d",name,surname,c);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
